d61af82b8c5644e399480c9b84efc3195a291e2d
andresmarino87 <andresmarino87@gmail.com>
2017-05-22 23:06:44 -0400

    Task model added

---
+++ server/api/task/task.model.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import {registerEvents} from './task.events';
+
+var TaskSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  milestones: Array
+});
+
+registerEvents(TaskSchema);
+export default mongoose.model('Task', TaskSchema);
